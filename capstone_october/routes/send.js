b45d5f6f59f82b23235e3a155190953aa1b93555
minhyuk Park <comon23463@gmail.com>
2019-11-25 22:38:06 +0900

    버튼수정

--- capstone_october/routes/send.js
+++ capstone_october/routes/send.js
@@ -13,11 +13,10 @@ router.post('/', function (req, res, next) {//투표 결과 처리
 
         if(err) console.log("에러의 유형은"+ err);
         for(let i =0; i<result.length; i++){
-            console.log('22222여기는 실행되곧있니?')
             client.messages.create({
                 to: result[i].p_number,
                 from:'+12057079411',
-                body:'곧 선거가 개설될 예정입니다 투표에 참여해주시기 바랍니다'
+                body:'선거가 개설 되었습니다. 투표에 참여해주시기 바랍니다'
             },function(err, message){
                 if(err){
                     console.log(err);
